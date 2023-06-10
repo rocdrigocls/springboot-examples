07a1db01d8ee3420455838c089a0351ea2a53ff8
rocdrigo <rocdrigo.cls@gmail.com>
2023-06-10 15:01:47 -0600

    1rst version

--- test_sqlserver-1/src/main/java/com/example/demo/TestSqlserver1Application.java
+++ test_sqlserver-1/src/main/java/com/example/demo/TestSqlserver1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestSqlserverApplication {
+public class TestSqlserver1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestSqlserverApplication.class, args);
+		SpringApplication.run(TestSqlserver1Application.class, args);
 	}
 
 }
